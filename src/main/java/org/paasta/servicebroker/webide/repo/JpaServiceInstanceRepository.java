5dfff68cf6a057cf1cd66387a7c7bc0e1a0cc73d
swmoon <swmoon@bluedigm.com>
2018-08-17 10:22:26 +0900

    WEB-IDE-BROKER Service INIT

--- src/main/java/org/paasta/servicebroker/webide/repo/JpaServiceInstanceRepository.java
+++ src/main/java/org/paasta/servicebroker/webide/repo/JpaServiceInstanceRepository.java
@@ -4,6 +4,10 @@ import org.paasta.servicebroker.webide.model.JpaServiceInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Created by sjchoi on 2018-08-17
+ */
+
 @Repository
 public interface JpaServiceInstanceRepository extends JpaRepository<JpaServiceInstance, String>{
     JpaServiceInstance findByOrganizationGuid(String organizationId);
